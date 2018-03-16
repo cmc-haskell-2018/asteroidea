6393371b9be18786f2535cc89d7976a68ffe094a
Haalon <Haalon@yandex.com>
2018-03-16 16:51:49 +0300

    VariationFunc=Params->(StdGen,Vec)->(StdGen,Vec) + fixed some errors

--- src/Types.hs
+++ src/Types.hs
@@ -1,38 +1,66 @@
 module Types where
+import Prelude --hiding ((.),id)
+--import Control.Category
 import System.Random
 import Graphics.Gloss
 
-data DPoint = DPoint (Double, Double) deriving (Show, Eq)
---уже есть в глоссе, но с флоатом?
+type VariationFunc =  Params -> (StdGen,Vec) -> (StdGen,Vec) --вместо Maybe Vec возможно стоит использовать Nan'ы 
+--Композиция работает
+--Например ((spherical None).(spherical None).(spherical None)) (defGen ,(1,2))  (что эквивалентно единичному применению сферического отображения)
+type Project = Vec ->  Double
+type Vec = (Double, Double) 
 
-type Variation = DPoint -> Maybe DPoint
-type Project = DPoint ->  Double
+{--
+newtype F a b = F (a->b)
+instance Category F where
+  id = F (\a -> a)
+  --(.) :: (F p) b c -> (F p) a b -> (F p) a c 
+  (.) (F bc) (F ab) = F (\a -> bc (ab a))
+--}
+  
+data Params = None | List [Double] | Matrix AffineMatrix
+--type Variation = (StdGen,Vec) -> (StdGen,Vec)
+--instance Category Variation where
+--(.) :: cat b c -> cat a b -> cat a c
+--  (.) (Variation g2 p2 v2) (Variation g p v) = 
 
-
+-- Возможно стоит хранить параметры вариаций вместе с ними самими в некой обертке?
+defGen = mkStdGen 42 --For debug purposes
 radius :: Project
-radius (DPoint (x,y)) = sqrt(x*x+y*y)
+radius (x,y) = sqrt(x*x +y*y)
+
+getNeigbours:: Double->Vec->[Vec]
+getNeigbours dl (x,y) = [v11,v12,v21,v22]
+  where
+    v11 = (x+dl,y+dl)
+    v12 = (x+dl,y-dl)
+    v21 = (x-dl,y+dl)
+    v22 = (x-dl,y-dl)
+
+nthNeigbours :: Integer->[Vec]
+nthNeigbours 0 = [(0,0)]
+nthNeigbours n = concat $ map (getNeigbours dl) (nthNeigbours (n-1))
+  where
+    dl = 2**(- (fromInteger n))
+
+biUnitTiling :: [Vec]
+biUnitTiling = concat  [ nthNeigbours i | i <- [0,1..]]
+
 
 -- примеры преобразований
--- | Сферическое преобразование
-spherical :: Variation
-spherical p@(DPoint (x,y))
-  | r/=0 = Just (DPoint (1/r*x, 1/r*y))
-  | otherwise = Nothing
+spherical :: VariationFunc
+spherical _ (gen ,p@(x,y))  = (gen, (1/r^2 *x, 1/r^2*y))
   where r = radius p
--- | Жулиа. Примеры преобразований
-juliaN ::
-  [Double]-> -- [julua Power, julua Distance]
-  Variation
-juliaN [power, dist] p@(DPoint(x,y))
-  = Just (DPoint(r**(dist/power)*(cos t) , r**(dist/power)*(sin t)))
+
+juliaN :: VariationFunc
+juliaN (List (power:dist:_)) (gen,p@(x,y)) = (gen, (r**(dist/power)*(cos t) , r**(dist/power)*(sin t)))
   where r = radius p
-        k = 0.5
-        -- k = unsafePerformIO (randomIO :: IO Double) -- нужен нормальный генератор случайных чисел на [0,1) 
+        k = fst $ (random gen) :: Double 
         p3 = fromIntegral $ truncate (k*power)
         t = ((atan2 y x) + 2*pi*p3)/power
 
-affineTransform :: AffineMatrix -> Variation
-affineTransform m (DPoint(x,y)) = Just (DPoint (xx m * x + xy m * y + ox m, yx m * x + yy m * y + oy m))
+affineTransform :: VariationFunc 
+affineTransform (Matrix m) (gen,(x,y)) = (gen, (xx m * x + xy m * y + ox m, yx m * x + yy m * y + oy m))
 
 data AffineMatrix = AffineMatrix {
 xx :: Double,
@@ -45,11 +73,9 @@ oy :: Double } deriving(Show)
 idMatrix :: AffineMatrix
 idMatrix = AffineMatrix 1 0 0 1 0 0
 
---узел\трансформация фракталы
 data Transform = Transform {
 transformName :: String,
-variation :: Variation,
--- возможны линейные комбинации, композиция, параметры =>
+variation :: Variation, -- возможны линейные комбинации, композиция, параметры =>
 -- variation :: [ [(Double, Variation)] ] ??
 weight :: Double,
 colorPosition :: Double,
@@ -57,10 +83,11 @@ colorSpeed :: Double,
 opacity :: Double,
 xaos :: [Double]
 }
--- | Глобальный фрактал
+
 data Model = Model {
   modelName :: String,
   tranforms :: [Transform],
   camera :: Maybe Transform,
   gradient :: [Color] 
+--position ?
 } 
