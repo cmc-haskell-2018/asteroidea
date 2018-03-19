729691574652922317f175eb10769cdde37611ab
Haalon <Haalon@yandex.com>
2018-03-19 22:43:43 +0300

    VTree

--- src/VarTree.hs
+++ src/VarTree.hs
@@ -19,8 +19,27 @@ type Operation = GVec->GVec->GVec
 type Path = [Int]
 data VTree = Node Operation [VTree] | Leaf Variation
 
+insertAt :: Int->a->[a]->[a]
+insertAt z y xs = as ++ (y:bs)
+                  where (as,bs) = splitAt z xs
+
 getSubTree :: VTree -> Path -> Maybe VTree
 getSubTree vt [] = Just vt
 getSubTree (Node _ l) (x:xs) | inRange l x =  getSubTree (l !! x) xs
                              | otherwise = Nothing
 getSubTree (Leaf _) _ = Nothing
+
+isValidTree :: VTree -> Bool
+isValidTree (Node _ []) = False
+isValidTree  (Leaf _) = True
+isValidTree (Node _ list) = and (map isValidTree list) 
+
+{-
+insertSubTree :: Path->VTree->VTree->VTree
+insertSubTree _ _ (Leaf v) = Leaf v
+insertSubTree _ subT (Node op []) = Node op [subT]
+insertSubTree [x] subt (Node op l) = Node op (insertAt x subt l)
+insertSubTree (x:xs) subt (Node op l) | x < 0 = Node op (insertAt 0 (insertSubTree xs subt (l !! 0)) l) 
+                                      | x >= length l = insertSubTree xs subt (l !! (length l - 1))
+                                      | otherwise = insertSubTree xs subt (l !! x)
+                                      -}
