fd0713378794703d24f4345fcab9314fcc411567
Haalon <Haalon@yandex.com>
2018-03-19 17:23:55 +0300

    VTree

--- src/VarTree.hs
+++ src/VarTree.hs
@@ -6,14 +6,14 @@ Stability   : in progress
 -}
 module VarTree where
 import Prelude  
-import System.Random
+--import System.Random
 import Types
 
-(!!!) :: [a]->Int->Maybe a
-(!!!) l i
-         | i < 0 = Nothing
-         | i >=length l = Nothing
-         | otherwise = Just $ l !! i
+inRange :: [a]->Int->Bool
+inRange l i
+         | i < 0 = False
+         | i >=length l = False
+         | otherwise = True
 
 type Operation = GVec->GVec->GVec
 type Path = [Int]
@@ -21,6 +21,6 @@ data VTree = Node Operation [VTree] | Leaf Variation
 
 getSubTree :: VTree -> Path -> Maybe VTree
 getSubTree vt [] = Just vt
-getSubTree (Node op l) (x:xs) | l !!! x == Nothing = Nothing 
-                              | otherwise = getSubTree (l !!! x) xs
+getSubTree (Node _ l) (x:xs) | inRange l x =  getSubTree (l !! x) xs
+                             | otherwise = Nothing
 getSubTree (Leaf _) _ = Nothing
