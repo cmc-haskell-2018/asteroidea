4d2dbc4d62d7f7bbb04b6849346ff61a77ce926f
Haalon <Haalon@yandex.com>
2018-03-19 17:10:37 +0300

    VTree

--- src/VarTree.hs
+++ src/VarTree.hs
@@ -9,6 +9,18 @@ import Prelude
 import System.Random
 import Types
 
+(!!!) :: [a]->Int->Maybe a
+(!!!) l i
+         | i < 0 = Nothing
+         | i >=length l = Nothing
+         | otherwise = Just $ l !! i
+
 type Operation = GVec->GVec->GVec
 type Path = [Int]
-data VTree = Node Operation [(Int,VTree)] | Leaf Variation
+data VTree = Node Operation [VTree] | Leaf Variation
+
+getSubTree :: VTree -> Path -> Maybe VTree
+getSubTree vt [] = Just vt
+getSubTree (Node op l) (x:xs) | l !!! x == Nothing = Nothing 
+                              | otherwise = getSubTree (l !!! x) xs
+getSubTree (Leaf _) _ = Nothing
