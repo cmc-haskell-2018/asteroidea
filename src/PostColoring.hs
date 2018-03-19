4997085b365ecfb52a1c11ef518b507a0c24edb4
Haalon <Haalon@yandex.com>
2018-03-19 13:52:52 +0300

    Oleg vs. Post Coloring

---
+++ src/PostColoring.hs
@@ -0,0 +1,15 @@
+{-|
+Module      : PostColoring
+Description : post coloring of the field
+Copyright   : Just Nothing
+Stability   : OLEG
+-}
+{--
+Для базы необходимы:
+Data для хранения всех параметров постобработки - гамма\вайбранси и прочие простые параметры, позже ргб кривые
+Собственно функция что принимает эти параметры, поле и выдает картинку/поле :: PostColorParams->Field->Field
+--}
+module PostColoring where
+
+import Graphics.Gloss
+import ClassField
