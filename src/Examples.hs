8e6915eb2ff76872dcbdb9c4bff7696cb4531419
Haalon <Haalon@yandex.com>
2018-03-19 13:55:25 +0300

    Examples module

---
+++ src/Examples.hs
@@ -0,0 +1,49 @@
+{-|
+Module      : Examples
+Description : for debug purposes
+Copyright   : Just Nothing
+Stability   : in progress
+-}
+module Examples where
+import Prelude  
+--import Control.Category
+import System.Random
+import Graphics.Gloss
+import Types
+
+-- | DEBUG sphere 1
+dbgSpherical1 :: Variation
+dbgSpherical1 = Var 1 None spherical
+-- | DEBUG sphere 2
+dbgSpherical2 :: Variation
+dbgSpherical2 = Var (-2) None spherical
+-- | DEBUG affine 1
+dbgAffine1 :: Variation
+dbgAffine1 = Var 1 (Matrix (AffineMatrix 0.5 0 0 0.5 0.5 0.5)) affineTransform
+-- | DEBUG affine 2
+dbgAffine2 :: Variation
+dbgAffine2 = Var 1 (Matrix (AffineMatrix 0 0.5 (-0.5) 0 (-0.5) (-0.5))) affineTransform
+-- | DEBUG affine 3
+dbgAffine3 :: Variation
+dbgAffine3 = Var 1 (Matrix (AffineMatrix 0.5 0 0 0.5 0.5 (-0.5))) affineTransform
+-- | DEBUG affine 4
+dbgAffine4 :: Variation
+dbgAffine4 = Var 1 (Matrix (AffineMatrix 0 0.5 (-0.5) 0 (-0.5) 0.5)) affineTransform
+
+t1 :: Transform
+-- ^ DEBUG transform 1
+t1 = Transform "t1" dbgAffine1 1 1 0 1 []
+t2 :: Transform
+-- ^ DEBUG transform 2
+t2 = Transform "t2" dbgAffine2 1 0.889 0 1 []
+t3 :: Transform
+-- ^ DEBUG transform 3
+t3 = Transform "t3" dbgAffine3 1 1 0 1 []
+t4 :: Transform
+-- ^ DEBUG transform 4
+t4 = Transform "t4" dbgAffine4 1 1 1 1 []
+
+exampleModel :: Model 
+exampleModel = Model "69" [t1,t2,t3,t4] Nothing grad 1024 1024 50 0
+  where
+   grad = [blue]
--- src/Variation.hs
+++ src/Variation.hs
@@ -22,25 +22,6 @@ defGen = mkStdGen 42
 calcVariation :: Variation -> (StdGen,Vec)-> (StdGen,Vec)
 calcVariation (Var s p f) a = s |*| (f p a)
 
--- | DEBUG sphere 1
-dbgSpherical1 :: Variation
-dbgSpherical1 = Var 1 None spherical
--- | DEBUG sphere 2
-dbgSpherical2 :: Variation
-dbgSpherical2 = Var (-2) None spherical
--- | DEBUG affine 1
-dbgAffine1 :: Variation
-dbgAffine1 = Var 1 (Matrix (AffineMatrix 0.5 0 0 0.5 0.5 0.5)) affineTransform
--- | DEBUG affine 2
-dbgAffine2 :: Variation
-dbgAffine2 = Var 1 (Matrix (AffineMatrix 0 0.5 (-0.5) 0 (-0.5) (-0.5))) affineTransform
--- | DEBUG affine 3
-dbgAffine3 :: Variation
-dbgAffine3 = Var 1 (Matrix (AffineMatrix 0.5 0 0 0.5 0.5 (-0.5))) affineTransform
--- | DEBUG affine 4
-dbgAffine4 :: Variation
-dbgAffine4 = Var 1 (Matrix (AffineMatrix 0 0.5 (-0.5) 0 (-0.5) 0.5)) affineTransform
-
 radius :: Project
 radius (x,y) = sqrt (x*x +y*y)
 -- | отображение в кожффициент потенциала в точке
@@ -64,24 +45,6 @@ juliaN _ a = a
 affineTransform :: VariationFunc 
 affineTransform (Matrix m) (gen,(x,y)) = (gen, (xx m * x + xy m * y + ox m, yx m * x + yy m * y + oy m))
 affineTransform _ a = a
-
-t1 :: Transform
--- ^ DEBUG transform 1
-t1 = Transform "t1" dbgAffine1 1 1 0 1 []
-t2 :: Transform
--- ^ DEBUG transform 2
-t2 = Transform "t2" dbgAffine2 1 0.889 0 1 []
-t3 :: Transform
--- ^ DEBUG transform 3
-t3 = Transform "t3" dbgAffine3 1 1 0 1 []
-t4 :: Transform
--- ^ DEBUG transform 4
-t4 = Transform "t4" dbgAffine4 1 1 1 1 []
-
-exampleModel :: Model 
-exampleModel = Model "69" [t1,t2,t3,t4] Nothing grad 1024 1024 50 0
-  where
-   grad = [blue]
 
 {--
    <palette count="256" format="RGB">
