b2eb369b91a7f5cf98d04907d2f8d93542c03755
alvadia <vadim1996a@hotmail.com>
2018-03-22 21:12:24 +0300

    Fetches

--- src/ClassField.hs
+++ src/ClassField.hs
@@ -6,7 +6,7 @@ Stability   : in progress
 -}
 module ClassField where
 
-import Graphics.Gloss
+import Graphics.Gloss()
 import System.Random
 import Data.Matrix
 import Types
@@ -121,3 +121,36 @@ merge :: Double -> UnsafeColour -> UnsafeColour
 merge _ col = mix (1,1,1) col -- asking gradient
   where
     mix (r,g,b) (t,h,n,s) = (r+t,g+h,b+n,s+1)
+
+-- | Выбор трансформы из списка
+-- нашей модели
+askTransform
+  :: Model -- ^ модель
+  -> Double-- ^ случайная величина [0,1)
+  -> Transform
+askTransform model choice =
+  let
+    thres = choice * (sumweight model)
+    list = transforms model
+  in findTrans (thres - (weight $ head list)) list
+-- | перебор по списку до первого не превосходящего порог
+findTrans :: Double -> [Transform] -> Transform
+findTrans thres (x:y:xs)
+  | (thres <= 0) = x
+  | otherwise = findTrans (thres - (weight y)) (y:xs)
+findTrans _ x = head x
+-- | Применение трансформы
+-- к цвету и вектору
+applyTransform
+  :: Transform
+  -> Double   -- ^ цвет в карте градиентов
+  -> GVec     -- ^ вектор
+  -> (GVec, Double)
+applyTransform transform colour gvec =
+  let
+    colSpeed = colorSpeed    transform
+    colPosit = colorPosition transform
+    newColour = (colour*abs(1+colSpeed) + colPosit*abs(1-colSpeed))/2
+    variatTr = variation transform
+    varFuncT = (function variatTr) $ (params variatTr)
+  in (varFuncT gvec,newColour)
--- src/Types.hs
+++ src/Types.hs
@@ -85,7 +85,7 @@ xaos :: [Double]
 data Model = Model {
   modelName :: String,
   -- | череда трансформ
-  tranforms :: [Transform],
+  transforms :: [Transform],
   -- viewPoint, условно
   camera :: Maybe Transform,
   -- | карта градиентов
@@ -98,5 +98,6 @@ data Model = Model {
   -- | зум
   mScale :: Double,
   -- | и поворот.
-  rotation :: Double
+  rotation :: Double,
+  sumweight :: Double
 }
