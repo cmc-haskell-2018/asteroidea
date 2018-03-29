8c8f8b0c7b41b58f042fbbfce91483427eb7a298
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-03-29 12:19:18 +0300

    Make nice equilateral Sierpinski gasket filling bi-unit square

--- src/Asteroidea/Sierpinski.hs
+++ src/Asteroidea/Sierpinski.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 module Asteroidea.Sierpinski where
 
 import qualified Asteroidea.IFS.Classic as Classic
@@ -7,8 +8,18 @@ import qualified Asteroidea.IFS.Classic as Classic
 -- 
 -- TODO: добавить сюда красивую картинку.
 sierpinskiGasket :: Classic.IFS
-sierpinskiGasket =
-  [ \(x, y) -> (x / 2,       y / 2)
-  , \(x, y) -> ((x + 1) / 2, y / 2)
-  , \(x, y) -> (x / 2, (y + 1) / 2)
+sierpinskiGasket = map f
+  [ \(x, y) -> (x / 2,         y / 2)
+  , \(x, y) -> ((x + 1) / 2,   y / 2)
+  , \(x, y) -> ((x + 0.5) / 2, (y + sqrt(3)/2) / 2)
   ]
+  where
+    -- преобразование из би-единичного квадрата в единичный и обратно
+    -- (чтобы треугольник был на весь экран)
+    f g = toBUS . g . fromBUS
+
+    -- преобразование из би-единичного квадрата в единичный
+    fromBUS (x, y) = ((x + 1) / 2, (y + 1) / 2)
+
+    -- преобразование из единичного квадрата в би-единичный
+    toBUS   (x, y) = (2 * x - 1, 2 * y - 1)
