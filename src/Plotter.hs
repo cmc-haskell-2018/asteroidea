6de0c70e4581946dfb87c651462d77a9da649189
alvadia <vadim1996a@hotmail.com>
2018-04-18 23:10:33 +0300

    no memory leak2018-04-18 23:10:33

--- src/Plotter.hs
+++ src/Plotter.hs
@@ -9,10 +9,12 @@ module Plotter (initField, updateField, linearFieldIndex) where
 import Types
 import Core
 import Data.Maybe
-import qualified Gradient
-import qualified Data.Vector.Unboxed as Vector
-import qualified Data.Vector.Unboxed.Mutable as Vector.Mutable
-import Control.Monad.ST (runST)
+import qualified Gradient                      (colorMap)
+import qualified Data.Vector.Unboxed as Vector (unsafeThaw, unsafeFreeze,generate)
+import qualified Data.Vector.Unboxed.Mutable as Mutable (modify)
+import qualified Control.Monad       as M      (fmap, mfilter,MonadPlus)
+import           Control.Monad.ST              (runST)
+import qualified Data.Foldable       as F      (mapM_)
 
 
 -- | Initialize field
@@ -25,118 +27,102 @@ initField m = Vector.generate (sizeX*sizeY) initFunction
 
 -- | Add points to the field
 updateField :: Model -> Field -> [CastGen]-> Field
-updateField m oldField points = foldl (plot m) oldField finalestPoints 
- where
-  finalPoints | isNothing $ mFinal m  = points
-              | otherwise             = map (applyFinal m) points
-  finalestPoints = map (\ (GVec g vec, c,i) -> (GVec g (applyCamera m vec), c,i)) finalPoints
- --finalestPoints = map (applyCamera m) finalPoints
-
-{-
--- | отрисовка точки на поле
-plot :: Model -> Field -> CastGen -> Field
-plot model field ((GVec g v@(x,y)), col, ptr)
-  | inBounds = newField
-  | otherwise = field
+updateField m oldField points =
+   newplot m finalestPoints oldField
   where
-    inBounds = control model x y
-    {-
-    shiftX, shiftY :: Double
-    shiftX = scaleFactor * mShiftX model
-    shiftY = scaleFactor * mShiftY model
-    -- rotation in radian
-    rotRad :: Double
-    rotRad = (pi/360*) $ mRotation model
-    -- sin / cos rotation multiplied on scaleFactor 
-    sinTheta, cosTheta :: Double
-    sinTheta = scaleFactor * sin rotRad
-    cosTheta = scaleFactor * cos rotRad
-    scaleFactor :: Double
-    scaleFactor = mScale model /50
-    x' = ((y*sinTheta + x*cosTheta) - shiftX)
-    y' = ((y*cosTheta - x*sinTheta) - shiftY)
-    
-    -}
-    setX = truncate ( ( x+1) * (fromIntegral $ mWidth  model)/2  ) 
-    setY = truncate ( (-y+1) * (fromIntegral $ mHeight model)/2  )
--- -y because y-axis direction is opposite of row number
-    coord = (setX, setY)
-    linearCoord = linearFieldIndex (mWidth model) coord
-    addedCol = Gradient.colorMap (mGradient model) col
-    newField = runST $ do 
-      mutableVector <- Vector.unsafeThaw field
-      Vector.Mutable.modify  mutableVector (calcColour addedCol) linearCoord 
-      updatedField <- Vector.unsafeFreeze mutableVector
-      return updatedField
-{-# INLINE plot #-}
--}
+    finalPoints | isNothing $ mFinal m  = points
+                | otherwise             = map (applyFinal m) points
+    finalestPoints =
+      map
+        (  \ (GVec g vec, c,i) -> ((applyCamera m vec), c, i) )
+        finalPoints
+
+-- | Функция размещения в поле ряда точек.
+-- Если я успею, то сделаю всё более красиво и понятно.
+newplot
+  :: 
+   ( M.MonadPlus  t
+   , Foldable     t
+   )
+  => Model
+  -> t (Vec,Double,Int)
+  -> Field
+  -> Field
+newplot model listCast field = let
+    listFieldPoints = produceListFromCasts model listCast
+  in runST $ do 
+     mutableVector <- Vector.unsafeThaw field
+                   -- TODO Effective implementation
+     _             <- F.mapM_ (
+                      ( (\f (c,v) -> f c v) )
+                      $ (Mutable.modify mutableVector)
+                           )
+                      $ listFieldPoints
+     updatedField  <- Vector.unsafeFreeze mutableVector
+     return updatedField
+
+-- | Генерация из списка Cast списка вида (mutate colour, position)
+-- для работы 'Vector.Mutable.modify'
+produceListFromCasts
+  :: M.MonadPlus t
+  => Model                -- ^ Параметры преобразований. Зачем я их таскаю?
+  -> t (Vec,Double,Int)   -- ^ Структура бросков. TODO effective
+  -> t (Cell -> Cell,Int) -- ^ Результат - функция-модификатор и индекс.
+produceListFromCasts model startList =
+  -- require Functor
+  M.fmap convert filteredList
+  where
+    convert (coord,col,ind) = (
+        (calcColour model ind)
+      . Gradient.colorMap
+        (mGradient            model)
+      $ col        
+      , linearFieldIndex
+        (mWidth               model)
+      . (pointBUStoFieldPoint model)
+      $ coord             )
+    filteredList =
+        -- require MonadPlus
+        M.mfilter (inBounds . (\(a,b,c)->a))
+        startList
+    inBounds (x,y) = (control x) && (control y)
+
+-- | convert index from virtual field as BUS to real field as Vector
+pointBUStoFieldPoint
+  :: Model            -- параметры модели
+  -> (Double, Double) -- точка из би-квадрата
+  -> (Int,Int)        -- точка на поле
+pointBUStoFieldPoint model =
+    \(x',y') ->
+       (
+         truncate $ scaleX *( x'+1)
+       , truncate $ scaleY *(-y'+1)
+       )
+  where 
+    scaleX = half $ mWidth  model
+    scaleY = half $ mHeight model
+    half x = (fromIntegral x) /2
+
 applyFinal :: Model -> CastGen -> CastGen
 applyFinal (Model {mFinal = Just final}) point = calcOne final point
 applyFinal _ c = c
 
 applyCamera :: Model -> Vec -> Vec
-applyCamera m (x,y) = (scalex,scaleY)
+applyCamera m (x,y) = (scaleX,scaleY)
   where
     (shiftX, shiftY) = (x+ mShiftX m, y+ mShiftY m)
-    rotRad = (2*pi/360*) $ mRotation m
+    rotRad = (pi/180*) $ mRotation m
     sinT = sin rotRad
     cosT = cos rotRad
     (rotX, rotY) = ( shiftX*cosT-shiftY*sinT, shiftY*cosT+shiftX*sinT)
-    (scalex,scaleY) =(rotX * mScale m,rotY * mScale m )
-
--- | отрисовка точки на поле
-plot :: Model -> Field -> CastGen -> Field
-plot model field ((GVec _ v@(x,y)), col, _)
-  | inBounds = newField
-  | otherwise = field
-  where
-    inBounds = control model v
-    setX = truncate ( ( x+1) * (fromIntegral $ mWidth  model)/2  ) 
-    setY = truncate ( (-y+1) * (fromIntegral $ mHeight model)/2  )
--- -y because y-axis direction is opposite of row number
-    coord = (setX, setY)
-    linearCoord = linearFieldIndex (mWidth model) coord
-    addedCol = Gradient.colorMap (mGradient model) col
-    newField = runST $ do 
-      mutableVector <- Vector.unsafeThaw field
-      Vector.Mutable.modify  mutableVector (calcColour addedCol) linearCoord 
-      updatedField <- Vector.unsafeFreeze mutableVector
-      return updatedField
-
-
--- | проверка что точка входит в поле
-control :: Model -> (Double,Double) -> Bool
--- не совсем верно - не учитывается зум и прочее
-control m (a,b) = not (cond halfX a || cond halfY b)
-  where
-    halfX = (fromIntegral $ mWidth m)/2
-    halfY = (fromIntegral $ mHeight m)/2
-    cond _ x = -- here was size
-      isNaN x ||
-      isInfinite x ||
-      x <= - 1 ||
-      x >= 1
-
-{-# INLINE control #-}
-
+    (scaleX,scaleY) =(rotX * mScale m,rotY * mScale m )
 
 -- | TODO alpha blending colours
-calcColour :: (Double,Double,Double) -> Cell -> Cell
-calcColour (r1,g1,b1) (r2, g2, b2, a) = ( (r2+r1), (g2+g1), (b2+b1), (a+1))
+calcColour :: Model -> Int -> (Double,Double,Double) -> Cell -> Cell
+calcColour _ _ (r1,g1,b1) (r2, g2, b2, a) = ( (r2+r1), (g2+g1), (b2+b1), (a+1))
 {-# INLINE calcColour #-}
 
-{-
--- | проверка что точка входит в поле
-control :: Model -> Double -> Double -> Bool
--- не совсем верно - не учитывается зум и прочее
-control m a b = not (cond halfX a || cond halfY b)
-  where
-    halfX = (fromIntegral $ mWidth m)/2
-    halfY = (fromIntegral $ mHeight m)/2
-    cond _ x = -- here was size
-      isNaN x ||
-      isInfinite x ||
-      x <= - 1 ||
-      x >=  1
+-- | проверка, что точка входит в поле (-1,1)
+control :: Double -> Bool
 {-# INLINE control #-}
--}
+control x = (x > - 1) && (x < 1)
