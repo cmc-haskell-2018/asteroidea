377522cffc67bdadea5bb8ec88af0deed79733cd
alvadia <vadim1996a@hotmail.com>
2018-04-21 19:37:05 +0300

    command line

--- app/Main.hs
+++ app/Main.hs
@@ -9,16 +9,37 @@ import Types
 import System.Random
 import Codec.Picture
 import Graphics.Gloss
-import qualified Model.Serpinski as M1
-import qualified Model.Serpinski0 as M0
-
+import Model.Serpinski as M0
+import Model.Serpinski0 as M1
 import Animation
+import qualified System.Environment as S
 
 main :: IO ()
-main = run
+main = do
+  genRand <-  newStdGen
+  let (seed, _) = next genRand 
+  commandArgs <- S.getArgs
+  case commandArgs of {
+       []            -> run
+    ;  (par:[])      -> case par of
+       "pic"         -> run
+       "interpol"    -> runAnim seed
+       _             -> run
+    ;  _             -> runAnim seed
+                      }
 
-runAnim :: IO ()
-runAnim = Animation.animate M0.mainModel M1.mainModel 60
+runAnim :: Int -> IO ()
+runAnim seed = sequence_
+        $ zipWith (\p im -> savePngImage p $ ImageRGBA8 im) paths
+        $ Animation.animate M0.mainModel M1.mainModel 60 seed
+  where
+    num = 60
+    paths = take (num+1) filenames
+    filenames = concat [l1,l2,l3]
+      where
+        l1 = map (\s -> "0"++"0"++ show s) $ [0,9::Int]
+        l2 = map (\s -> "0"     ++ show s) $ [10,99::Int]
+        l3 = map show [100::Int,101..]
 
 run :: IO ()
 run = do 
@@ -27,7 +48,6 @@ run = do
   let field = createField M1.mainModel  $ calcFlame M1.mainModel seed 
   let img = generateImage (fieldCellToPixel M1.mainModel field) (mWidth M1.mainModel) (mHeight M1.mainModel)
   let pic = fromImageRGBA8 img
-  
   savePngImage  "./pic.png" (ImageRGBA8  img) 
   Graphics.Gloss.animate window white (\_->pic)
   where       
