5a41f26d1fc036fe53dcece37bc417c9eda67b48
oxot <tutushkinart@yandex.ru>
2018-04-23 21:50:16 +0300

    added parseTran

--- src/Parser.hs
+++ src/Parser.hs
@@ -9,33 +9,43 @@ module Parser where
 import Types 
 import Variations	
 import Gradient
-
-splitt :: Char -> String -> [String]
-splitt _ "" = []
-splitt c s = firstWord : (splitt c rest)
- where firstWord = takeWhile (/=c) s
-       rest = drop (length firstWord + 1) s
+import Data.List.Split
 
 -- contents <- readFile "a.txt"
 -- words $ contents
 
+-- | read a model from the file
 parseModel :: [String] -> Model -> Model  
 parseModel (fName : fVal : rest) mod 
-    | (fName == "mName")             = (parseModel rest mod {mName = fVal})
- -- | (fName == "mTransforms")       = 
- -- | (fName == "mFinal")            =
- 	| (fName == "mGradient")         = (parseModel rest mod {mGradient = paletteToDouble fVal})
- 	| (fName == "mWidth")            = (parseModel rest mod {mWidth = read fVal :: Int})
- 	| (fName == "mHeight")           = (parseModel rest mod {mHeight = read fVal :: Int})
- 	| (fName == "mScale")            = (parseModel rest mod {mScale = read fVal :: Double})
- 	| (fName == "mShiftX")           = (parseModel rest mod {mShiftX = read fVal :: Double})
- 	| (fName == "mShiftY")           = (parseModel rest mod {mShiftY = read fVal :: Double})
- 	| (fName == "mRotation")         = (parseModel rest mod {mRotation = read fVal :: Double})
- 	| (fName == "mBackgroundColour") = (parseModel (tail $ tail $ rest) mod {mBackgroundColour = (\_ -> (read fVal :: Double, read (head rest) :: Double, read (head $ tail $ rest) :: Double, 1))})
- 	| (fName == "mOuterIter")        = (parseModel rest mod {mOuterIter = read fVal :: Int})
- 	| (fName == "mInnerIter")        = (parseModel rest mod {mInnerIter = read fVal :: Int})
+    | (fName == "mName")             = parseModel rest mod {mName = fVal}
+    | (fName == "mTransforms")       = let 
+                                       l = (splitOn ["endT"] (fVal : rest))
+                                       trans = map (parseTran templateTransform) (init l)
+                                       r = last l 
+    								   in parseModel r mod {mTransforms = trans}
+    | (fName == "mFinal")            = let (t:r:[]) = (splitOn ["endF"] (fVal : rest)) 
+									   in parseModel r mod {mFinal = Just (parseTran templateTransform t )}
+    | (fName == "mGradient")         = parseModel rest mod {mGradient = paletteToDouble fVal}
+    | (fName == "mWidth")            = parseModel rest mod {mWidth = read fVal :: Int}
+    | (fName == "mHeight")           = parseModel rest mod {mHeight = read fVal :: Int}
+    | (fName == "mScale")            = parseModel rest mod {mScale = read fVal :: Double}
+    | (fName == "mShiftX")           = parseModel rest mod {mShiftX = read fVal :: Double}
+    | (fName == "mShiftY")           = parseModel rest mod {mShiftY = read fVal :: Double}
+    | (fName == "mRotation")         = parseModel rest mod {mRotation = read fVal :: Double}
+    | (fName == "mBackgroundColour") = parseModel (drop 2 rest) mod {mBackgroundColour = (\_ -> (read fVal :: Double, read (head rest) :: Double, read (head $ tail $ rest) :: Double, 1))}
+    | (fName == "mOuterIter")        = parseModel rest mod {mOuterIter = read fVal :: Int}
+    | (fName == "mInnerIter")        = parseModel rest mod {mInnerIter = read fVal :: Int}
+    | otherwise                      = mod
 
---parseTrans :: String -> [Transform]
+-- | parse a single transform
+parseTran :: Transform -> [String] -> Transform
+parseTran tran (fName : fVal : rest)
+  -- | (fName == "tVariation") =
+  	| (fName == "tWeight") = parseTran tran {tWeight = read fVal :: Double} rest
+  	| (fName == "tColorPosition") = parseTran tran {tColorPosition = read fVal :: Double} rest
+  	| (fName == "tColorSpeed") = parseTran tran {tColorSpeed = read fVal :: Double} rest
+  	| (fName == "tOpacity") = parseTran tran {tOpacity = read fVal :: Double} rest
+  	-- | (fName == "tXaos") = 
 
 --parseVar :: String -> Variation
 
